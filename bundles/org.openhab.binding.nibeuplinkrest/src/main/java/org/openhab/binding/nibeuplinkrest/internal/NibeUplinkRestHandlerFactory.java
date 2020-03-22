fd80974d22a47535f9a63e3e2d18b681258face1
Anders Alfredsson <andersb86@gmail.com>
2020-03-22 09:01:34 +0100

    Add Nibeuplink Rest binding
    Signed-off-by: Anders Alfredsson <andersb86@gmail.com>

---
+++ bundles/org.openhab.binding.nibeuplinkrest/src/main/java/org/openhab/binding/nibeuplinkrest/internal/NibeUplinkRestHandlerFactory.java
@@ -0,0 +1,127 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.nibeuplinkrest.internal;
+
+import static org.openhab.binding.nibeuplinkrest.internal.NibeUplinkRestBindingConstants.*;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.jetty.client.HttpClient;
+import org.openhab.core.auth.client.oauth2.OAuthFactory;
+import org.openhab.core.thing.Bridge;
+import org.openhab.core.thing.Thing;
+import org.openhab.core.thing.ThingTypeUID;
+import org.openhab.core.thing.binding.BaseThingHandlerFactory;
+import org.openhab.core.thing.binding.ThingHandler;
+import org.openhab.core.thing.binding.ThingHandlerFactory;
+import org.openhab.core.thing.link.ItemChannelLinkRegistry;
+import org.openhab.core.io.net.http.HttpClientFactory;
+import org.openhab.binding.nibeuplinkrest.internal.auth.NibeUplinkRestOAuthService;
+import org.openhab.binding.nibeuplinkrest.internal.handler.NibeUplinkRestBaseSystemHandler;
+import org.openhab.binding.nibeuplinkrest.internal.handler.NibeUplinkRestBridgeHandler;
+import org.openhab.binding.nibeuplinkrest.internal.handler.NibeUplinkRestThermostatHandler;
+import org.openhab.binding.nibeuplinkrest.internal.provider.NibeUplinkRestTypeFactory;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * The {@link NibeUplinkRestHandlerFactory} is responsible for creating things and thing
+ * handlers.
+ *
+ * @author Anders Alfredsson - Initial contribution
+ */
+@NonNullByDefault
+@Component(service = ThingHandlerFactory.class, configurationPid = "binding.nibeuplinkrest")
+public class NibeUplinkRestHandlerFactory extends BaseThingHandlerFactory {
+
+    private static final Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Collections
+            .unmodifiableSet(Stream.of(THING_TYPE_APIBRIDGE, THING_TYPE_SYSTEM, THING_TYPE_THERMOSTAT)
+                    .collect(Collectors.toSet()));
+
+    private @NonNullByDefault({}) OAuthFactory oAuthFactory;
+    private @NonNullByDefault({}) NibeUplinkRestOAuthService oAuthService;
+    private @NonNullByDefault({}) HttpClient httpClient;
+    private @NonNullByDefault({}) NibeUplinkRestTypeFactory typeFactory;
+    private @NonNullByDefault({}) ItemChannelLinkRegistry itemChannelLinkRegistry;
+
+    @Override
+    public boolean supportsThingType(ThingTypeUID thingTypeUID) {
+        return SUPPORTED_THING_TYPES_UIDS.contains(thingTypeUID) || thingTypeUID.getBindingId().equals(BINDING_ID);
+    }
+
+    @Override
+    protected @Nullable ThingHandler createHandler(Thing thing) {
+        ThingTypeUID thingTypeUID = thing.getThingTypeUID();
+
+        if (THING_TYPE_APIBRIDGE.equals(thingTypeUID)) {
+            final NibeUplinkRestBridgeHandler handler = new NibeUplinkRestBridgeHandler
+                    ((Bridge) thing, oAuthFactory, httpClient, typeFactory);
+            oAuthService.addBridgeHandler(handler);
+            return handler;
+        }
+        if (THING_TYPE_THERMOSTAT.equals(thingTypeUID)) {
+            return new NibeUplinkRestThermostatHandler(thing, itemChannelLinkRegistry);
+        }
+        if (thingTypeUID.getBindingId().equals(BINDING_ID)) {
+            return new NibeUplinkRestBaseSystemHandler(thing, typeFactory.getGroupTypeProvider());
+        }
+
+        return null;
+    }
+
+    @Reference
+    protected void setOAuthFactory(OAuthFactory oAuthFactory) {
+        this.oAuthFactory = oAuthFactory;
+    }
+
+    protected void unsetOAuthFactory(OAuthFactory oAuthFactory) {
+        this.oAuthFactory = null;
+    }
+
+    @Reference
+    protected void setHttpClientFactory(HttpClientFactory httpClientFactory) {
+        this.httpClient = httpClientFactory.getCommonHttpClient();
+    }
+
+    protected void unsetHttpClientFactory(HttpClientFactory httpClientFactory) {
+        this.httpClient = null;
+    }
+
+    @Reference
+    protected void setOAuthService(NibeUplinkRestOAuthService oAuthService) { this.oAuthService = oAuthService; }
+
+    protected void unsetOAuthService(NibeUplinkRestOAuthService oAuthService) { this.oAuthService = null; }
+
+    @Reference
+    protected void setTypeFactory(NibeUplinkRestTypeFactory typeFactory) {
+        this.typeFactory = typeFactory;
+    }
+
+    protected void unsetTypeFactory(NibeUplinkRestTypeFactory typeFactory) {
+        this.typeFactory = null;
+    }
+
+    @Reference
+    protected void setItemChannelLinkRegistry(ItemChannelLinkRegistry itemChannelLinkRegistry) {
+        this.itemChannelLinkRegistry = itemChannelLinkRegistry;
+    }
+
+    protected void unsetItemChannelLinkRegistry(ItemChannelLinkRegistry itemChannelLinkRegistry) {
+        this.itemChannelLinkRegistry = null;
+    }
+}
---
+++ bundles/org.openhab.binding.nibeuplinkrest/src/main/java/org/openhab/binding/nibeuplinkrest/internal/api/NibeUplinkRestResponseParser.java
@@ -0,0 +1,247 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
+package org.openhab.binding.nibeuplinkrest.internal.api;
+
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.binding.nibeuplinkrest.internal.api.model.*;
+import org.openhab.binding.nibeuplinkrest.internal.util.StringConvert;
+
+import java.lang.reflect.Type;
+import java.time.ZonedDateTime;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author Anders Alfredsson - Initial contribution
+ */
+@NonNullByDefault
+public class NibeUplinkRestResponseParser {
+
+    /**
+     * Custom deserializer to handle ZonedDateTime
+     */
+    private static class ZonedDateTimeDeserializer implements JsonDeserializer<ZonedDateTime> {
+        @Override
+        public ZonedDateTime deserialize(@Nullable JsonElement json, @Nullable Type type,
+                                         @Nullable JsonDeserializationContext jsonDeserializationContext)
+                throws JsonParseException {
+            if (json == null)
+            {
+                throw new JsonParseException("null");
+            }
+            return ZonedDateTime.parse(json.getAsJsonPrimitive().getAsString());
+        }
+    }
+
+    /**
+     * Custom deserializer to handle Mode
+     */
+    private static class ModeDeserializer implements JsonDeserializer<Mode> {
+        @Override
+        public Mode deserialize(@Nullable JsonElement json, @Nullable Type type,
+                                         @Nullable JsonDeserializationContext jsonDeserializationContext)
+                throws JsonParseException {
+            if (json == null)
+            {
+                throw new JsonParseException("null");
+            }
+            return Mode.from(json.getAsJsonObject().get("mode").getAsString());
+        }
+    }
+
+    /**
+     * Custom deserializer to get system info in a nested json object
+     */
+    private static class NibeSystemListDeserializer implements JsonDeserializer<List<NibeSystem>> {
+
+        @Override
+        public List<NibeSystem> deserialize(@Nullable JsonElement json, @Nullable Type type,
+                                            @Nullable JsonDeserializationContext jsonDeserializationContext)
+                throws JsonParseException {
+            if (json == null)
+            {
+                throw new JsonParseException("null");
+            }
+            JsonElement inner = json.getAsJsonObject().get("objects");
+
+            return new GsonBuilder().registerTypeAdapter(ZonedDateTime.class, new ZonedDateTimeDeserializer())
+                    .create().fromJson(inner, new TypeToken<List<NibeSystem>>(){}.getType());
+        }
+    }
+
+    /**
+     * Custom deserializer to get alarm info in a nested json object
+     */
+    private static class AlarmInfoDeserializer implements JsonDeserializer<AlarmInfo> {
+
+        @Override
+        public AlarmInfo deserialize(@Nullable JsonElement json, @Nullable Type type,
+                                            @Nullable JsonDeserializationContext jsonDeserializationContext)
+                throws JsonParseException {
+            if (json == null)
+            {
+                throw new JsonParseException("null");
+            }
+
+            return new Gson().fromJson(json.getAsJsonObject().get("info"), AlarmInfo.class);
+        }
+    }
+
+    /**
+     * Custom deserializer to get a list of alarm info in a nested json object
+     */
+    private static class AlarmInfoListDeserializer implements JsonDeserializer<List<AlarmInfo>> {
+
+        @Override
+        public List<AlarmInfo> deserialize(@Nullable JsonElement json, @Nullable Type type,
+                                     @Nullable JsonDeserializationContext jsonDeserializationContext)
+                throws JsonParseException {
+            if (json == null)
+            {
+                throw new JsonParseException("null");
+            }
+
+            JsonElement objects = json.getAsJsonObject().get("objects");
+
+            return new Gson().fromJson(objects, new TypeToken<List<AlarmInfo>>(){}.getType());
+        }
+    }
+
+    private static class SoftwareInfoDeserializer implements JsonDeserializer<SoftwareInfo> {
+
+        @Override
+        public SoftwareInfo deserialize(@Nullable JsonElement json, @Nullable Type type,
+                                            @Nullable JsonDeserializationContext jsonDeserializationContext)
+                throws JsonParseException {
+            if (json == null)
+            {
+                throw new JsonParseException("null");
+            }
+            JsonElement current = json.getAsJsonObject().get("current").getAsJsonObject().get("name");
+            JsonElement upgrade = json.getAsJsonObject().get("upgrade");
+            String upgradeVersion = upgrade.isJsonNull() ? null : upgrade.getAsJsonObject().get("name").getAsString();
+
+            return new SoftwareInfo(current.getAsString(), upgradeVersion);
+        }
+    }
+
+    private static Gson gson = new GsonBuilder()
+            .registerTypeAdapter(ZonedDateTime.class, new ZonedDateTimeDeserializer())
+            .registerTypeAdapter(Mode.class, new ModeDeserializer())
+            .registerTypeAdapter(SoftwareInfo.class, new SoftwareInfoDeserializer())
+            .registerTypeAdapter(new TypeToken<List<NibeSystem>>(){}.getType(), new NibeSystemListDeserializer())
+            .registerTypeAdapter(AlarmInfo.class, new AlarmInfoDeserializer())
+            .registerTypeAdapter(new TypeToken<List<AlarmInfo>>(){}.getType(), new AlarmInfoListDeserializer())
+            .create();
+
+    /**
+     * Parse a json string with system info
+     *
+     * @param json
+     * @return
+     */
+    public static NibeSystem parseSystem(String json) {
+        return gson.fromJson(json, NibeSystem.class);
+    }
+
+    /**
+     * Parse a json string with a list of systems
+     *
+     * @param json
+     * @return
+     */
+    public static List<NibeSystem> parseSystemList(String json) {
+        return gson.fromJson(json, new TypeToken<List<NibeSystem>>(){}.getType());
+    }
+
+    /**
+     * Parse a json string with status info
+     *
+     * @param json
+     * @return
+     */
+    public static Set<String> parseStatus(String json) {
+        Set<String> activeComponents = new HashSet<>();
+        JsonParser parser = new JsonParser();
+        JsonArray items = parser.parse(json).getAsJsonArray();
+        items.forEach((item) -> {
+            String title = item.getAsJsonObject().get("title").getAsString();
+            activeComponents.add(StringConvert.toCamelCase(title));
+        });
+        return activeComponents;
+    }
+    /**
+     * Parse a json string with notification info
+     *
+     * @param json
+     * @return
+     */
+    public static List<AlarmInfo> parseAlarmInfoList(String json) {
+        return gson.fromJson(json, new TypeToken<List<AlarmInfo>>(){}.getType());
+    }
+
+    /**
+     * Parse a json string with system config information
+     *
+     * @param json
+     * @return
+     */
+    public static SystemConfig parseSystemConfig(String json) {
+        return gson.fromJson(json, SystemConfig.class);
+    }
+
+    /**
+     * Parse a json string with software info
+     *
+     * @param json
+     * @return
+     */
+    public static SoftwareInfo parseSoftwareInfo(String json) {
+        return gson.fromJson(json, SoftwareInfo.class);
+    }
+
+    /**
+     * Parse a json string with a list of categories
+     *
+     * @param json
+     * @return
+     */
+    public static List<Category> parseCategoryList(String json) {
+        return gson.fromJson(json, new TypeToken<List<Category>>(){}.getType());
+    }
+
+    /**
+     * Parse a json string with a list of parameters
+     *
+     * @param json
+     * @return
+     */
+    public static List<Parameter> parseParameterList(String json) {
+        return gson.fromJson(json, new TypeToken<List<Parameter>>(){}.getType());
+    }
+
+    /**
+     * Parse a json string with info on the mode of the system
+     *
+     * @param json
+     * @return
+     */
+    public static Mode parseMode(String json) {
+        return gson.fromJson(json, Mode.class);
+    }
+}
