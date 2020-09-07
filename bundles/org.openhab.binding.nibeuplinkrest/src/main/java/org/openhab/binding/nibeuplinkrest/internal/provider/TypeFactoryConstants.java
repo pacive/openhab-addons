50a0ba32756692ef84000a1d0d6ccef65ef85213
Anders Alfredsson <andersb86@gmail.com>
2020-09-07 18:16:07 +0200

    Improve and fix after OH3 port
    Signed-off-by: Anders Alfredsson <andersb86@gmail.com>
    (cherry picked from commit 179c4b459722c9ce7a9a7a2971945b5eeac2c1e2)

--- bundles/org.openhab.binding.nibeuplinkrest/src/main/java/org/openhab/binding/nibeuplinkrest/internal/provider/TypeFactoryConstants.java
+++ bundles/org.openhab.binding.nibeuplinkrest/src/main/java/org/openhab/binding/nibeuplinkrest/internal/provider/TypeFactoryConstants.java
@@ -18,8 +18,6 @@ import static org.openhab.binding.nibeuplinkrest.internal.NibeUplinkRestBindingC
 import java.net.URI;
 import java.util.*;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.openhab.core.thing.type.ChannelTypeUID;
@@ -58,43 +56,32 @@ public class TypeFactoryConstants {
     public static final int SCALE_FACTOR_HUNDRED = 100;
 
     // Map of static type mappings, for parameters that cannot be classified automatically
-    public static final Map<Integer, ParameterType> STATIC_PARAMETER_TYPE_MAPPINGS = Collections.unmodifiableMap(Stream
-            .of(new AbstractMap.SimpleEntry<>(43416, ParameterType.COUNTER),
-                    new AbstractMap.SimpleEntry<>(43371, ParameterType.BOOLEAN),
-                    new AbstractMap.SimpleEntry<>(43372, ParameterType.BOOLEAN),
-                    new AbstractMap.SimpleEntry<>(43064, ParameterType.TEMPERATURE),
-                    new AbstractMap.SimpleEntry<>(43065, ParameterType.TEMPERATURE),
-                    new AbstractMap.SimpleEntry<>(43124, ParameterType.AIRFLOW),
-                    new AbstractMap.SimpleEntry<>(40050, ParameterType.AIRFLOW),
-                    new AbstractMap.SimpleEntry<>(47407, ParameterType.STRING),
-                    new AbstractMap.SimpleEntry<>(47408, ParameterType.STRING),
-                    new AbstractMap.SimpleEntry<>(47409, ParameterType.STRING),
-                    new AbstractMap.SimpleEntry<>(47410, ParameterType.STRING),
-                    new AbstractMap.SimpleEntry<>(47411, ParameterType.STRING),
-                    new AbstractMap.SimpleEntry<>(47412, ParameterType.STRING))
-            .collect(Collectors.toMap(AbstractMap.SimpleEntry::getKey, AbstractMap.SimpleEntry::getValue)));
+    public static final Map<Integer, ParameterType> STATIC_PARAMETER_TYPE_MAPPINGS = Map.ofEntries(
+            Map.entry(43416, ParameterType.COUNTER), Map.entry(43371, ParameterType.BOOLEAN),
+            Map.entry(43372, ParameterType.BOOLEAN), Map.entry(43064, ParameterType.TEMPERATURE),
+            Map.entry(43065, ParameterType.TEMPERATURE), Map.entry(43124, ParameterType.AIRFLOW),
+            Map.entry(40050, ParameterType.AIRFLOW), Map.entry(47407, ParameterType.STRING),
+            Map.entry(47408, ParameterType.STRING), Map.entry(47409, ParameterType.STRING),
+            Map.entry(47410, ParameterType.STRING), Map.entry(47411, ParameterType.STRING),
+            Map.entry(47412, ParameterType.STRING));
 
     // Static scale factor mappings for parameters that need it
-    public static final Map<Integer, Integer> STATIC_SCALING_FACTORS = Collections.unmodifiableMap(Stream
-            .of(new AbstractMap.SimpleEntry<>(43125, SCALE_FACTOR_TEN),
-                    new AbstractMap.SimpleEntry<>(43136, SCALE_FACTOR_TEN))
-            .collect(Collectors.toMap(AbstractMap.SimpleEntry::getKey, AbstractMap.SimpleEntry::getValue)));
+    public static final Map<Integer, Integer> STATIC_SCALING_FACTORS = Map.of(43125, SCALE_FACTOR_TEN, 43136,
+            SCALE_FACTOR_TEN);
 
     // Parameter id mappings for the heating control channels
-    public static final Map<String, List<Integer>> HEAT_CONTROL_PARAMETERS = Collections.unmodifiableMap(Stream
-            .of(new AbstractMap.SimpleEntry<>(CHANNEL_PARALLEL_ADJUST_HEAT_ID,
-                    Arrays.asList(0, 47011, 47010, 47009, 47008, 48494, 48493, 48492, 48491)),
-                    new AbstractMap.SimpleEntry<>(CHANNEL_PARALLEL_ADJUST_COOL_ID,
-                            Arrays.asList(0, 48739, 48738, 48737, 48736, 48735, 48734, 48733, 48732)),
-                    new AbstractMap.SimpleEntry<>(CHANNEL_TARGET_TEMP_HEAT_ID,
-                            Arrays.asList(0, 47398, 47397, 47396, 47395, 48683, 48682, 48681, 48680)),
-                    new AbstractMap.SimpleEntry<>(CHANNEL_TARGET_TEMP_COOL_ID,
-                            Arrays.asList(0, 48785, 48784, 48783, 48782, 48781, 48780, 48779, 48778)))
-            .collect(Collectors.toMap(AbstractMap.SimpleEntry::getKey, AbstractMap.SimpleEntry::getValue)));
+    public static final Map<String, List<String>> HEAT_CONTROL_PARAMETERS = Map.of(CHANNEL_PARALLEL_ADJUST_HEAT_ID,
+            List.of("47011", "47010", "47009", "47008", "48494", "48493", "48492", "48491"),
+            CHANNEL_PARALLEL_ADJUST_COOL_ID,
+            List.of("48739", "48738", "48737", "48736", "48735", "48734", "48733", "48732"),
+            CHANNEL_TARGET_TEMP_HEAT_ID,
+            List.of("47398", "47397", "47396", "47395", "48683", "48682", "48681", "48680"),
+            CHANNEL_TARGET_TEMP_COOL_ID,
+            List.of("48785", "48784", "48783", "48782", "48781", "48780", "48779", "48778"));
 
     public static final String HEAT_CONTROL_CHANNEL_LABEL = "System %s %s";
 
-    public static final List<ChannelTypeUID> STATUS_CHANNELS = Arrays.asList(CHANNEL_TYPE_COMPRESSOR,
+    public static final List<ChannelTypeUID> STATUS_CHANNELS = List.of(CHANNEL_TYPE_COMPRESSOR,
             CHANNEL_TYPE_VENTILATION, CHANNEL_TYPE_ADDITION, CHANNEL_TYPE_HEATING_MEDIUM_PUMP, CHANNEL_TYPE_HOT_WATER,
             CHANNEL_TYPE_HEATING, CHANNEL_TYPE_COOLING, CHANNEL_TYPE_LAST_ACTIVITY, CHANNEL_TYPE_HAS_ALARMED,
             CHANNEL_TYPE_ALARM_INFO, CHANNEL_TYPE_SOFTWARE_UPDATE, CHANNEL_TYPE_LATEST_SOFTWARE);
