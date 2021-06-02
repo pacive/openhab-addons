b9b9b42d76c794282f78158432518c7201dbe194
Anders Alfredsson <andersb86@gmail.com>
2021-06-02 13:37:32 +0200

    Fix som checkstyle INFOs
    Signed-off-by: Anders Alfredsson <andersb86@gmail.com>

--- bundles/org.openhab.binding.nibeuplinkrest/src/main/java/org/openhab/binding/nibeuplinkrest/internal/api/model/Parameter.java
+++ bundles/org.openhab.binding.nibeuplinkrest/src/main/java/org/openhab/binding/nibeuplinkrest/internal/api/model/Parameter.java
@@ -97,10 +97,12 @@ public class Parameter {
 
     @Override
     public boolean equals(@Nullable Object o) {
-        if (this == o)
+        if (this == o) {
             return true;
-        if (o == null || getClass() != o.getClass())
+        }
+        if (o == null || getClass() != o.getClass()) {
             return false;
+        }
         Parameter parameter = (Parameter) o;
         return parameterId == parameter.parameterId;
     }
